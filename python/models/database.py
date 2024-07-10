e8e86cf5fcd58369f7c2f3e17baa76363e456a82
Álvaro Cardeña <valo.ac91@gmail.com>
2024-07-10 12:51:49 -0600

    Create: database.py

---
+++ python/models/database.py
@@ -0,0 +1,16 @@
+from .connection_manager import ConnectionManager
+
+
+# Integración de las clases referente a la obtención de datos de la base de datos
+# para proporcionar una interfaz centralizada.
+class Database:
+    def __init__(self, host, database, user, password, port, driver) -> None:
+        self.connection_manager = ConnectionManager(host, database, user, password, port, driver)
+        self.connection = None
+    
+    def connect(self):
+        if self.connection_manager.connect():
+            self.connection = self.connection_manager.get_connection()
+
+    def close(self):
+        self.connection_manager.close()
