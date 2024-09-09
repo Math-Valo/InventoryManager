e279c697e00585d27dfe0f948581d0755abfa0bf
Álvaro Cardeña <valo.ac91@gmail.com>
2024-09-09 17:01:45 -0600

    Create: get_modify_solution_window.py

--- python/views/get_modify_solution_window.py
+++ python/views/get_modify_solution_window.py
@@ -1,10 +1,10 @@
 import os
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QDialogButtonBox, QApplication, QPushButton, QFileDialog
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QDialogButtonBox, QPushButton, QFileDialog, QApplication
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
 
-class ModifySolutionWindow(QWidget):
+class GetModifySolutionWindow(QWidget):
     def __init__(self) -> None:
         super().__init__()
         self.button_box = QDialogButtonBox(QDialogButtonBox.Yes | QDialogButtonBox.No)
@@ -14,7 +14,7 @@ class ModifySolutionWindow(QWidget):
         self.setup_ui()
 
     def setup_ui(self):
-        self.setWindowTitle("Confirmar cambios manuales")
+        self.setWindowTitle('Cargar cambios')
         self.setGeometry(100, 100, 400, 200)
 
         # Layout principal
@@ -27,13 +27,13 @@ class ModifySolutionWindow(QWidget):
         font.setWeight(50)
 
         # Etiqueta con la pregunta
-        question_label = QLabel("¿Desea realizar cambios antes de continuar?")
+        question_label = QLabel("¿Desea usar los niveles finales de la tabla modificada?")
         question_label.setFont(font)
         question_label.setAlignment(Qt.AlignCenter)
         layout.addWidget(question_label)
 
-        # Etiqueta informativa para la ubicación donde se guardará el archivo
-        path_label = QLabel("El archivo se guardará en la siguiente ubicación:")
+        # Etiqueta informativa para la ubicación donde se cargará el archivo
+        path_label = QLabel("El archivo se guardará desde la siguiente ubicación:")
         layout.addWidget(path_label)
 
         # Etiqueta informativa para la ubicación del archivo
@@ -42,7 +42,7 @@ class ModifySolutionWindow(QWidget):
         layout.addWidget(self.path_display)
 
         # Botón para seleccionar la ubicación de guardado
-        self.button_select_path.setText("Cambiar ubicación de guardado")
+        self.button_select_path.setText("Cambiar ubicación de carga")
         layout.addWidget(self.button_select_path)
 
         # Botones de respuestas
@@ -57,6 +57,6 @@ class ModifySolutionWindow(QWidget):
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
-    window = ModifySolutionWindow()
+    window = GetModifySolutionWindow()
     window.show()
     sys.exit(app.exec_())
